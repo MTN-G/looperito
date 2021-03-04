e1c0cbfec1db7095934332c678bb63f6a9ccfd1a
Matan Greenvlad <mgk2798@gmail.com>
2021-03-04 21:44:36 +0200

    make grid responsive

--- looperito/src/components/looper.js
+++ looperito/src/components/looper.js
@@ -2,13 +2,14 @@ import React, { useEffect, useState } from "react";
 import Pad from "./pad";
 import PlayArrowIcon from "@material-ui/icons/PlayArrow";
 import PauseCircleOutlineSharpIcon from "@material-ui/icons/PauseCircleOutlineSharp";
+import { Grid } from "@material-ui/core";
 
 function Looper({ allSounds }) {
   // const [activeSounds, setActiveSounds] = useState([]);
   const [playing, setPlaying] = useState(false);
 
   return (
-    <div id="looper">
+    <div className="looper">
       <button
         className="playbtn"
         onClick={() => {
@@ -17,16 +18,20 @@ function Looper({ allSounds }) {
       >
         {playing ? <PauseCircleOutlineSharpIcon /> : <PlayArrowIcon />}
       </button>
-      <div id="pads-grip">
-        {allSounds &&
-          allSounds.map((sound) => (
-            <Pad
-              sound={sound}
-              // activeSounds={activeSounds}
-              // setActiveSounds={setActiveSounds}
-              playing={playing}
-            />
-          ))}
+      <div id={"pads-grid"}>
+        <Grid container spacing={3}>
+          {allSounds &&
+            allSounds.map((sound) => (
+              <Grid item xs={12} sm={6} md={4} style={{ textAlign: "center" }}>
+                <Pad
+                  sound={sound}
+                  // activeSounds={activeSounds}
+                  // setActiveSounds={setActiveSounds}
+                  playing={playing}
+                />
+              </Grid>
+            ))}
+        </Grid>
       </div>
     </div>
   );
